3a3e1893b4973a1ea6ab93571e4612ba4de30291
unknown <vishnuvpk05@gmail.com>
2024-02-22 17:54:07 +0530

    refactor: clean

--- backend/crud.py
+++ backend/crud.py
@@ -3,27 +3,10 @@ from fastapi import HTTPException,APIRouter
 
 
 crud_router = APIRouter()
-
-@crud_router.post("/insert-data/")
-async def insert_data():
-    try:
-        connection = connect_to_database()
-        if connection:
-            cursor = connection.cursor()
-            query = "INSERT INTO `bolt_db`.`doctor` (`user_id`, `name`, `specialization`, `email`, `phone_no`, `patients`, `availability`) VALUES ('1', '2', '3', '4', '5', '6', '7');"
-            cursor.execute(query)
-            connection.commit()
-            connection.close()
-            return {"message": "Data inserted successfully"}
-        else:
-            raise HTTPException(status_code=500, detail="Unable to connect to database")
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
     
 @crud_router.post("/CrudTest")
 async def crudTest():
     return {"message": "Test POST call From CRUD APIs"}
-
 
 #POST APIs to Insert User Accounts
 #1. Doctor Info
