efa3ca0de81cef66bdb5360843e956cc9881ba31
Sashank Ravipati <sashankr04@gmail.com>
2024-02-23 12:11:44 +0530

    feat: GenAI based Diet/Exercise/Report Generation, black reformatting

--- frontend/website/views.py
+++ frontend/website/views.py
@@ -2,36 +2,98 @@ from flask import Blueprint, render_template, request
 from flask_login import login_user, login_required, logout_user, current_user
 import requests
 
-views = Blueprint('views', __name__)
+views = Blueprint("views", __name__)
 
-@views.route('/')
+
+@views.route("/")
 @login_required
 def home():
-    return render_template("home.html",user=current_user)
+    return render_template("home.html", user=current_user)
 
-@views.route('/patient-dashboard',methods=['GET','POST'])
+
+@views.route("/patient-dashboard", methods=["GET", "POST"])
 @login_required
 def patient():
-    if request.method == 'POST':
-        health_data = {
-            'user_id':int(current_user.user_id),
-            'pulserate': request.form.get('pulse_rate'),
-            'syspressure': request.form.get('pulse_rate'),
-            'diapressure': request.form.get('dia_pressure'),
-            'bloodsugar': request.form.get('blood_sugar'),
-            'stepcount':request.form.get('step_count')
-        }
+    response = requests.get(
+    f"http://localhost:3000/vitals_fetch/{current_user.user_id}")
+    if response.ok:
+        vitals_data = response.json().get("Vitals")
+    else:
+        vitals_data = None
 
-        response = requests.post('http://localhost:3100/vitals_upload/', json=health_data)
-        print(response)
-        if response.ok:
-            return 'Health data submitted successfully!'
-        else:
-            return 'Error submitting health data.'
-    return render_template("patient.html",user=current_user)
+    if request.method == "POST":
+        if request.form["submit_button"] == "Vitals":
+            health_data = {
+                "user_id": int(current_user.user_id),
+                "pulserate": request.form.get("pulse_rate"),
+                "syspressure": request.form.get("sys_pressure"),
+                "diapressure": request.form.get("dia_pressure"),
+                "bloodsugar": request.form.get("blood_sugar"),
+                "stepcount": request.form.get("step_count"),
+            }
+
+            response = requests.post(
+                "http://localhost:3000/vitals_upload/", json=health_data
+            )
+            if (
+                response.json().get("Error")
+                == "Vitals can be posted only once per day."
+            ):
+                return "Vitals can only be posted once per day. Please try again later."
+            elif response.ok:
+                return "Health data submitted successfully!"
+            else:
+                return "Error submitting health data."
+
+        if request.form["submit_button"] == "Diet":
+            response = requests.get(
+                f"http://localhost:3000/vitals_fetch/{current_user.user_id}"
+            )
+            if response.ok:
+                vitals_data = response.json().get("Vitals")[0]
+                response = requests.post(
+                    "http://localhost:3000/dietPlan/", json=vitals_data
+                )
+                if response.ok:
+                    result = response.json().get("message")
+                    return result
+                else:
+                    return "Error fetching diet plan."
+
+        if request.form["submit_button"] == "Exercise":
+            response = requests.get(
+                f"http://localhost:3000/vitals_fetch/{current_user.user_id}"
+            )
+            if response.ok:
+                vitals_data = response.json().get("Vitals")[0]
+                response = requests.post(
+                    "http://localhost:3000/exercisePlan/", json=vitals_data
+                )
+                if response.ok:
+                    result = response.json().get("message")
+                    return result
+                else:
+                    return "Error fetching exercise plan."
+
+        if request.form["submit_button"] == "EODReport":
+            response = requests.get(
+                f"http://localhost:3000/vitals_fetch/{current_user.user_id}"
+            )
+            if response.ok:
+                vitals_data = response.json().get("Vitals")[0]
+                response = requests.post(
+                    "http://localhost:3000/EODReport/", json=vitals_data
+                )
+                if response.ok:
+                    result = response.json().get("message")
+                    return result
+                else:
+                    return "Error fetching Report."
+
+    return render_template("patient.html", user=current_user, vitals=vitals_data)
 
 
-@views.route('/doctor-dashboard')
+@views.route("/doctor-dashboard")
 @login_required
 def doctor():
-    return render_template("doctor.html",user=current_user)
+    return render_template("doctor.html", user=current_user)
